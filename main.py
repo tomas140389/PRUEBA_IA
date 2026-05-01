237fa55de612f7cbcac9a69152532b039e991927
tomas140389 <tomgc17@gmail.com>
2026-05-01 14:53:55 -0600

    V1

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import requests
+import pandas as pd
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"status": "ok"}
+
+@app.get("/procesar")
+def procesar():
+    data = requests.get("https://jsonplaceholder.typicode.com/posts/1").json()
+
+    resultado = [{
+        "titulo": data["title"],
+        "contenido": data["body"]
+    }]
+
+    df = pd.DataFrame(resultado)
+    archivo = "resultado.xlsx"
+    df.to_excel(archivo, index=False)
+
+    return FileResponse(archivo)
