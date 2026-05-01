7e0c1aa00f484f2644594220ce82060d7668bbf1
tomas140389 <tomgc17@gmail.com>
2026-05-01 15:19:23 -0600

    V4

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import requests
 import pandas as pd
 from fastapi.responses import FileResponse
 
+
+return FileResponse(
+    archivo,
+    media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    filename="resultado.xlsx"
+)
+
 app = FastAPI()
 
 @app.get("/")
@@ -22,4 +29,40 @@ def procesar():
     archivo = "resultado.xlsx"
     df.to_excel(archivo, index=False)
 
-    return FileResponse(archivo)
+    from fastapi import FastAPI
+import requests
+import pandas as pd
+from fastapi.responses import FileResponse
+
+
+return FileResponse(
+    archivo,
+    media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    filename="resultado.xlsx"
+)
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"status": "ok"}
+
+@app.get("/procesar")
+def procesar():
+    data = requests.get("https://jsonplaceholder.typicode.com/posts/1").json()
+
+    resultado = [{
+        "titulo": data["title"],
+        "contenido": data["body"]
+    }]
+
+    df = pd.DataFrame(resultado)
+    archivo = "resultado.xlsx"
+    df.to_excel(archivo, index=False)
+
+    return FileResponse(
+        archivo,
+        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        filename="resultado.xlsx"
+    )
+
